c6319928915d0ce033b562a751aceac6ba3597c3
Joshua Waters <watersjoshua2002@gmail.com>
2023-10-30 11:18:53 +0000

    lab 2-4

---
+++ rocketbot-basic.py
@@ -0,0 +1,99 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import wikipedia
+import json, requests
+APIkey = "5403a1e0442ce1dd18cb1bf7c40e776f" 
+import aiml
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+# Create a Kernel object. No string encoding (all I/O is unicode)
+kern = aiml.Kernel()
+kern.setTextEncoding(None)
+# Use the Kernel's bootstrap() method to initialize the Kernel. The
+# optional learnFiles argument is a file (or list of files) to load.
+# The optional commands argument is a command (or list of commands)
+# to run after the files are loaded.
+# The optional brainFile argument specifies a brain file to load.
+kern.bootstrap(learnFiles="rockets-basic.xml")
+
+df = pd.read_csv('rockets.csv')
+questions = df['query'].tolist()
+answers = df['description'].tolist()
+
+# TF-IDF Vectorizer
+vectorizer = TfidfVectorizer().fit_transform(questions)
+
+def get_similar_question(user_input):
+    query_vector = vectorizer.transform([user_input])
+    similarity = cosine_similarity(query_vector, vectorizer)
+    index = similarity.argmax()
+    return answers[index]
+
+print("Welcome to this chat bot. Please feel free to ask questions from me!")
+
+while True:
+    try:
+        userInput = input("> ")
+    except (KeyboardInterrupt, EOFError) as e:
+        print("Bye!")
+        break
+    #pre-process user input and determine response agent (if needed)
+    responseAgent = 'aiml'
+    #activate selected response agent
+    if responseAgent == 'aiml':
+        answer = kern.respond(userInput)
+        #post-process the answer for commands
+        if answer[0] == '#':
+            params = answer[1:].split('$')
+            if params[0] == 'SpaceXAPI':
+                url = "https://api.spacexdata.com/v4/rockets"
+                response = requests.get(url)
+                if response.status_code == 200:
+                    rockets = response.json()
+                    print("---------------------------------------------------")
+                    for rocket in rockets:
+                        print(f"Rocket Name: {rocket['name']}")
+                        print(f"Description: {rocket['description']}")
+                        print(f"Height: {rocket['height']['meters']} meters")
+                        print(f"Diameter: {rocket['diameter']['meters']} meters")
+                        print(f"Mass: {rocket['mass']['kg']} kg")
+                        print("---------------------------------------------------")
+                else:
+                    print(f"Failed to fetch data. HTTP Status Code: {response.status_code}")
+            else:
+                cmd = int(params[0])
+                if cmd == 0:
+                    print(params[1])
+                    break
+                elif cmd == 1:
+                    try:
+                        wSummary = wikipedia.summary(params[1], sentences=3,auto_suggest=False)
+                        print(wSummary)
+                    except:
+                        print("Sorry, I do not know that. Be more specific!")
+                elif cmd == 2:
+                    succeeded = False
+                    api_url = r"http://api.openweathermap.org/data/2.5/weather?q="
+                    response = requests.get(api_url + params[1] + r"&units=metric&APPID="+APIkey)
+                    if response.status_code == 200:
+                        response_json = json.loads(response.content)
+                        if response_json:
+                            t = response_json['main']['temp']
+                            tmi = response_json['main']['temp_min']
+                            tma = response_json['main']['temp_max']
+                            hum = response_json['main']['humidity']
+                            wsp = response_json['wind']['speed']
+                            wdir = response_json['wind']['deg']
+                            conditions = response_json['weather'][0]['description']
+                            print("The temperature is", t, "°C, varying between", tmi, "and", tma, "at the moment, humidity is", hum, "%, wind speed ", wsp, "m/s,", conditions)
+                            succeeded = True
+                    if not succeeded:
+                        print("Sorry, I could not resolve the location you gave me.")
+                elif cmd == 99:
+                    print("I did not get that, please try again.")
+        else:
+            # If AIML doesn't have an answer, use the similarity-based component
+            if answer == "":  # or whatever default response AIML gives when it doesn't know the answer
+                answer = get_similar_question(userInput)
+            print(answer)
